bf15560e629a6c3b82fec88a6e92ce62282ddd6c
Julien Sarda <jsarda@made-f0Br7s17.clusters.42paris.fr>
2024-11-28 17:02:07 +0100

    fix

--- ex00/ScalarConverter.hpp
+++ ex00/ScalarConverter.hpp
@@ -1,11 +1,11 @@
 #pragma once
 
 #include <iostream>
+#include <limits.h>
 #include <limits>
 #include <cstdlib>
 #include <iomanip>
-#include <cmath>
-#include <sstream>
+#include <cmath> 
 
 enum Type { CHAR, INT, FLOAT, DOUBLE, SPECIAL, UNKNOWN };
 
@@ -28,11 +28,6 @@ class ScalarConverter {
 		static Type getType(std::string literal);
 		static void convertFromChar(char c);
 		static void convertFromInt(int i);
-		static void convertFromFloat(std::string literal);
-		static void convertFromDouble(std::string literal);
-
-		static char strToChar(const std::string literal);
-		static int strToInt(const std::string literal);
-		static float strToFloat(const std::string literal);
-		static double strToDouble(const std::string literal);
+		static void convertFromFloat(float f);
+		static void convertFromDouble(double d);
 };
